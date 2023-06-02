af31c1b48283a5704197fae875000c467d7956bd
AkshayChemuri <chemuriakshay@gmail.com>
2023-06-02 08:16:19 -0500

    added polymorphismStruc code

---
+++ src/polymorphismStruc.java
@@ -0,0 +1,29 @@
+class Cricket{
+    public void playerPlay(){
+        System.out.println("Players will play cricket");
+    }
+}
+
+class Football extends Cricket{
+    public void playerPlay(){
+        System.out.println("Players will play football");
+    }
+}
+class Tennis extends Football{
+    public void playerPlay(){
+        System.out.println("Players will play Tennis");
+    }
+}
+
+
+public class polymorphismStruc{
+    public static void main(String[] args){
+        Cricket c=new Cricket();
+        Cricket c1=new Football();
+        Cricket c2=new Tennis();
+        c.playerPlay();
+        c1.playerPlay();
+        c2.playerPlay();
+
+    }
+}
