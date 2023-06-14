07c9e7912668ea6c6f486e4e58c6ca1dec84ae09
AkshayChemuri <chemuriakshay@gmail.com>
2023-06-14 14:58:16 -0500

    Day 12 assignment

---
+++ src/com/fullstack/javacore/week3/Day12/UsingRunnable.java
@@ -0,0 +1,18 @@
+package com.fullstack.javacore.week3.Day12;
+
+class NumberPrinter implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+public class UsingRunnable {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new NumberPrinter());
+        thread.start();
+
+    }
+}
