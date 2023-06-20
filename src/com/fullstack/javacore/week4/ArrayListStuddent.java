40fea071e1b8ff8a2bd868d004bcc48309f01f00
AkshayChemuri <chemuriakshay@gmail.com>
2023-06-20 14:47:29 -0500

    week 4 Assignments

---
+++ src/com/fullstack/javacore/week4/ArrayListStuddent.java
@@ -0,0 +1,32 @@
+package com.fullstack.javacore.week4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListStuddent {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("CIS");
+        stringList.add("Kesh");
+        stringList.add("Akshay");
+        stringList.add("Kumar");
+
+        // Print the list before removing the last object
+        System.out.println("List before removing the last object:");
+        System.out.println(stringList);
+
+        // Remove the last object
+        int lastIndex = stringList.size() - 1;
+        if (lastIndex >= 0) {
+            stringList.remove(lastIndex);
+            System.out.println("\n Last index removed.");
+        } else {
+            System.out.println("\n  No index to remove it is empty.");
+        }
+
+        // Print the list after removing the last object
+        System.out.println("\nList after removing the last object:");
+        System.out.println(stringList);
+    }
+}
+
---
+++ src/com/fullstack/javacore/week4/ArrayListToArrayExample.java
@@ -0,0 +1,23 @@
+package com.fullstack.javacore.week4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListToArrayExample {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Akshay");
+        stringList.add("Kumar");
+        stringList.add("CIS");
+        stringList.add("Kesh");
+
+        // Convert ArrayList to Array
+        String[] stringArray = new String[stringList.size()];
+        stringArray = stringList.toArray(stringArray);
+        // Print the converted array
+        for (String element : stringArray) {
+            System.out.println(element);
+        }
+    }
+}
+
