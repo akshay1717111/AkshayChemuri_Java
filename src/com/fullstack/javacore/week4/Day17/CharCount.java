307cdfc112298210d6083aef14cd592956bc5bc1
AkshayChemuri <chemuriakshay@gmail.com>
2023-06-21 12:01:12 -0500

    added new folder

---
+++ src/com/fullstack/javacore/week4/Day17/CharCount.java
@@ -0,0 +1,25 @@
+package com.fullstack.javacore.week4.Day17;
+
+import java.util.HashMap;
+
+public class CharCount {
+    public static void printDistinctCharacters(String str) {
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
+        String emptyString = " ";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            charCountMap.put(ch, charCountMap.getOrDefault(ch, 0) + 1);
+        }
+
+        for (Character ch : charCountMap.keySet()) {
+            int count = charCountMap.get(ch);
+            System.out.println(ch + "-" + count);
+        }
+    }
+
+    public static void main(String[] args) {
+        String input = "test string";
+        printDistinctCharacters(input);
+    }
+}
+
